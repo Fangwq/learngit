439aa4c1990de4a926e71ff4b15ba6168719e6bc
FWQ <shenzhizi124@gmail.com>
2017-02-13 23:29:37 +0800

    revise a litttle

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def find(x)
+def find(x):
 	if parent[x] != x:
 		parent[x] = find(parent[x])
 	return parent[x]
 
-def rank(x): 
+def rank(x):
 	count = 1
 	while parent[x] != x:
 		count = count+1
 		x = parent[x]
 	return count
 
-def  union(x, y):
+def union(x, y):
 	x_root = find(x)
 	y_root = find(y)
 	if x_root == y_root:   #in same set
@@ -24,6 +24,8 @@ def  union(x, y):
 			rank[y_root] = rank[y_root] + 1
 	else:
 		parent[x_root] = y_root
+	return x+y
+
 
 #this method need to know the node and its parent. In function find, 
 #the recursion line realize the path compression process. The rank can be 
