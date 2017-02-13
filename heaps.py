f6d89dd9c38a1456a25d0490184d51b512400858
FWQ <shenzhizi124@gmail.com>
2017-02-13 11:00:30 +0800

    add delete and insert method

--- heaps.py
+++ heaps.py
@@ -5,9 +5,9 @@ class structure_heap(object):
     '''I handel the problem in heap data structure'''
     def __init__(self, array):
         self._array=array
-        self._len=len(array)
 
     def siftup(self, heap, num):
+        '''sift up the element whose key is greater than the key of its parent.'''
         count=0
         if num==0:
             return heap
@@ -24,8 +24,9 @@ class structure_heap(object):
         return heap
 
     def siftdown(self, heap, num):
+        '''sift down the element whose key is smaller than the key of its max son.'''
         count=0
-        if 2*num > len(heap):
+        if 2*num >= len(heap):
             return heap
         else:
             while 2*num <= len(heap):
@@ -38,3 +39,27 @@ class structure_heap(object):
                     heap[num], heap[(num-1)/2] = heap[(num-1)/2], heap[num]
         # print count
         return heap
+
+    def insert(self, heap, x):
+        '''insert an element in a heap'''
+        num=len(heap)
+        heap=np.hstack([heap, x])
+        self.siftup(heap,num)
+
+        return heap
+
+    def delete(self, heap, num):
+        '''delete an element in a heap'''
+        length=len(heap)
+        x=heap[num]; y=heap[length-1]
+        length=length-1
+        if num==length:
+            # print '===='
+            return heap[0:length]
+        heap[num]=y
+        if y > x:
+            self.siftup(heap,num)
+        else:
+            self.siftdown(heap, num)
+        return heap[0:length]
+
--- heaps_main.py
+++ heaps_main.py
@@ -2,6 +2,17 @@
 import numpy as np
 from heaps import structure_heap
 
-a=np.array([20,17,9,10,11,4,5,3,7,25])
-b_heap=structure_heap(a,9)
-print b_heap.siftup(a,9)
+a0=np.array([20,17,9,10,11,4,5,3,7,25])
+b0_heap=structure_heap(a0)
+print b0_heap.siftup(a0,9)
+a1=np.array([20,3,9,10,11,4,5,3,7,5])
+b1_heap=structure_heap(a1)
+print b0_heap.siftdown(a1,1)
+a2=np.array([20,17,9,10,11,4,5,3,7,5])
+b2_heap=structure_heap(a2)
+print b2_heap.insert(a2,30)
+a3=np.array([20,17,9,10,11,4,5,3,7,5])
+b3_heap=structure_heap(a3)
+print b3_heap.delete(a3,9)
+
+
