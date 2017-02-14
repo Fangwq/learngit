ade38c1f86b774e941e5ac0130f85c871d53ea20
FWQ <shenzhizi124@gmail.com>
2017-02-14 22:24:18 +0800

    wait for modification

--- linkedlist.py
+++ linkedlist.py
@@ -17,29 +17,73 @@ class Node(object):
 			node = node.pointer
 		return count
 
-class linkedlist(Node):
-	def __init__(self, header = None):
-		self._header = header
+class linkedlist(object):
+	def __init__(self):
+		# super(linkedlist,self).__init__()
+		self._header = None
 
 	def is_empty(self):
 		'''check if it is empty'''
-		if self._head is None:
+		if self._header is None:
 			return 'the list is empty'
 		else:
-			return 'the lsit is not empty'
+			return 'the list is not empty'
 
 	def prepend(self, elem):
-		'''insert an element in the head'''
-		self._header = Node(elem)
+		'''append an element in the head'''
+		self._header = Node(elem, self._header)
 
-	def eppend(self, end, elem):
-		'''eppend an element in the end'''
-		while end.pointer is not None:
-			end = end.pointer
-		end.pointer = Node(elem)
+	def endpend(self, elem):
+		'''append an element in the end'''
+		if self._header is None:
+			self._header = Node(elem)
+			return
+		p = self._header
+		while p is not None:
+			p = p.pointer
+		p.pointer = Node(elem)   #p.pointer.pointer = None
 
-	def pop(self, elem):
-		if 
+	def pop(self):
+		if self._header is None:
+			raise Exception("the list is empty")
+		e = self._header.elem
+		self._header = self._header.pointer
+		return e
+
+	def pop_last(self):
+		if self._header is None:
+			raise Exception("the list is empty")
+		p = self._header
+		if p.pointer is None:  #If only one element
+			e = p.elem
+			self._header=None
+			return e
+		while p.pointer.pointer is not None:    #find the last
+			p = p.pointer
+		e = p.pointer.elem
+		p.pointer = None
+		return e
+
+	def find(self, pred):
+		p = self._header
+		count = 0
+		while pred != p.elem and p is not None:
+			count = count+1
+			p = p.pointer
+		if count==self._header.length(self._header)-1:
+			print 'the pred is not in the list'
+		return count
+
+	def print_all(self):
+		p = self._header
+		while p is not None:
+			print p.elem,', ',   #, for not line break
+			p = p.pointer
+		print ''
+
+
+
+
 
 llist = Node(1)
 # llist.pointer=Node(2)
