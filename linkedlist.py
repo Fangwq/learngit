8cca5fd2adbb33475e2c0377b0de13a1b48b3023
FWQ <shenzhizi124@gmail.com>
2017-02-14 17:28:22 +0800

    creat node and list

--- linkedlist.py
+++ linkedlist.py
@@ -5,35 +5,41 @@ class Node(object):
 	'''each node contains an element and the pointer 
 		direct to the next element.
 	'''
-	def __init__(self, head, elem, pointer=None):
+	def __init__(self, elem, pointer = None):
 		self.elem = elem
 		self.pointer = pointer
 
-	def is_empty(self):
-		'''check if it is empty'''
-		if self.pointer is None:
-			return 'the list is empty'
-		else:
-			return 'the lsit is not empty'
-
-	def length(self, head):   #point is an example of class Node, and it let me think of recurrence
+	def length(self, point):   #point is an example of class Node, and it let me think of recurrence
 		'''the length of of the linked list'''
-		count, node = 0, head
+		count, node = 0, point
 		while node is not None:
 			count = count+1
 			node = node.pointer
 		return count
 
-	def prepend(self, head, elem):
-		'''insert an element in head'''
-		temp = Node(elem)
-		temp.pointer = head.pointer
-		head = temp
+class linkedlist(Node):
+	def __init__(self, header = None):
+		self._header = header
+
+	def is_empty(self):
+		'''check if it is empty'''
+		if self._head is None:
+			return 'the list is empty'
+		else:
+			return 'the lsit is not empty'
+
+	def prepend(self, elem):
+		'''insert an element in the head'''
+		self._header = Node(elem)
 
 	def eppend(self, end, elem):
+		'''eppend an element in the end'''
 		while end.pointer is not None:
 			end = end.pointer
 		end.pointer = Node(elem)
+
+	def pop(self, elem):
+		if 
 
 llist = Node(1)
 # llist.pointer=Node(2)
