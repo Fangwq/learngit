6cbf0df52d8d7abe12eaa056b77f3f4137412280
FWQ <shenzhizi124@gmail.com>
2017-02-17 23:03:41 +0800

    just for a try

--- recurrence.py
+++ recurrence.py
@@ -13,14 +13,14 @@ a1 = copy.deepcopy(a0)
 print 'before sort:', a0
 b0_method = recurrence_sort(a0)
 b0_sample = b0_method.rec_SelectSort(0)
-print 'after sort:',b0_sample
+print 'after sort:', b0_sample
 print 'before sort:', a1
 b1_method = recurrence_sort(a1)
 b1_sample = b1_method.rec_InsertSort(0)
 print 'after sort:', b1_sample
 plt.figure()
-plt.plot(xrange(count),b0_sample,'ro')
-plt.plot(xrange(count),b1_sample,'g+')
+plt.plot(xrange(count), b0_sample, 'ro')
+plt.plot(xrange(count), b1_sample, 'g+')
 plt.xlim(-1, count+1)
 plt.ylim(-1.05, 1.05)
 plt.show()
