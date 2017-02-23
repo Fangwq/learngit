27899ce3be3cfe41462a6b537c567fd4e8cfade6
FWQ <shenzhizi124@gmail.com>
2017-02-23 20:26:05 +0800

    add merge sort

--- di_con_main.py
+++ di_con_main.py
@@ -3,15 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from divide_conquer import divideConquer
-# import copy
+import copy
 
-count = 2**5
+count = 2**7
 a0 = np.random.rand(count)*2.0-1.0 
-# a1 = copy.copy(a0)
+a2 = copy.copy(a0)
 b0_method = divideConquer(a0)
 b0_sample = b0_method.minmax(0,count-1, a0)
 print b0_sample,min(a0),max(a0)
+# ======================================
 a1 = np.array([1, 4, 6, 9, 10 ])
 b1_method = divideConquer(a1)
-b1_sample = b0_method.binarysearch(0, len(a1)-1, a1, 12)
+b1_sample = b1_method.binarysearch(0, len(a1)-1, a1, 12)
 print b1_sample
+# ======================================
+print 'before sort',a2
+b2_method = divideConquer(a2)
+b2_sample = b2_method.mergesort(0, count-1, a2)
+print b2_sample
+plt.figure()
+plt.plot(xrange(count),b2_sample,'ro')
+plt.show()
--- divide_conquer.py
+++ divide_conquer.py
@@ -36,6 +36,45 @@ class divideConquer(object):
 			else:
 				return self.binarysearch(mid+1, high, array, x)
 
+	def bottomup_merge(self, templist, p, q, r):
+		'''merge method used by bottomup sort'''
+		# templist = self._sortlist
+		tempB = np.zeros(r-p+1)		#use this method,the memory used will be smaller.
+		s = p-1; t = q; k = p-1
+		while s <= q-1 and t <= r-1:
+			if templist[s] <= templist[t]:
+				tempB[k-(p-1)] = templist[s]
+				s=s+1
+			else:
+				tempB[k-(p-1)] = templist[t]
+				t=t+1
+			k=k+1
+		if s==q:
+			for i in xrange(k, r):
+				tempB[i-(p-1)] = templist[t]
+				t=t+1
+			# print "t={0}, r={1}".format(t,r),'===='
+		else:
+			for i in xrange(k, r):
+				tempB[i-(p-1)] = templist[s]
+				s=s+1
+			# print "s={0}, q={1}".format(s,q),'####'
+		for i in xrange(p-1,r):
+			templist[i] = tempB[i-(p-1)]
+		# print 'the auxiliary array:', tempB
+		return templist[p-1:r]
+
+
+	def mergesort(self, low, high, array):
+		if low < high:
+			mid = (low+high-1)/2
+			self.mergesort(low, mid, array)
+			self.mergesort(mid+1, high, array)
+			self.bottomup_merge(array, low+1, mid+1, high+1)
+		return array
+
+
+
 
 
 
