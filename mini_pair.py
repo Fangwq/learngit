a335a68a575804e24da706ec15309cdc2ff740de
FWQ <shenzhizi124@gmail.com>
2017-03-01 23:37:46 +0800

    check the algorithm

--- mini_pair.py
+++ mini_pair.py
@@ -12,7 +12,7 @@ class closest_pair(object):
 	def distance(self, a, b):
 		return np.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 )
 
-	def cp(self, low, high, x_array, y_array):
+	def cp(self, low, high, x_array, y_array):   #how to get which point is closest?
 		# temp = copy.copy(array)
 		length = len(x_array)
 		if high - low + 1 <= 3:
@@ -60,29 +60,44 @@ class closest_pair(object):
 				if abs(y_array[i][0] - x0) <= detal:
 					k = k + 1
 					T.append(y_array[i])
-			# print T,'######'
-			prime_detal = 2*detal
+			# print k,'######'
+			prime_detal = 2.0*detal
 			for i in xrange(k-1):
 				for j in xrange(i+1, min(i+7, k)):
 					dis = self.distance(T[i], T[j])
 					if dis < prime_detal:
 						prime_detal = dis
-						temp_xpoint = i            #temp_xpoint is the index in y_array
-						temp_ypoint = j
+						xindex = i            #xindex is the index in T
+						yindex = j
+			# print xindex, yindex, prime_detal
 			detal = min(detal, prime_detal)
 			index = np.argmin([detal, prime_detal])
 			if index != 0:                         #find the index in x_array
-				x = T[temp_xpoint]
-				y = T[temp_ypoint]
+				print '========='
+				x = T[xindex]
+				y = T[yindex]
 				xpoint = x_array.index(x)
 				ypoint = x_array.index(y)
-				# print xpoint, ypoint
-			# print detal, '===='
 		return detal, xpoint, ypoint
 
-count = 10
+	def brute_force(self, array):
+		num = len(array)
+		mini = 1000.0
+		for i in xrange(num-1):
+			for j in xrange(i+1, num):
+				x = array[i]
+				y = array[j]
+				dis = self.distance(x, y)
+				if dis < mini:
+					mini = dis
+					xpoint = i 
+					ypoint = j
+		return mini, xpoint, ypoint
+
+np.random.seed(1337) 
+count = 15
 alpha = np.random.rand(count)*10.0 - 5.0
-beta = np.random.rand(count)*7.0 - 5.0
+beta = np.random.rand(count)*10.0 - 5.0
 rand = []
 for i in xrange(count):
 	rand.append([alpha[i],beta[i]])
@@ -94,15 +109,20 @@ temp.sort(key=lambda x: x[1])   		#sort by y
 Y = temp
 # print X
 # print Y
-r = closest_pair(rand)
+r = closest_pair(X)
 result = r.cp(0, count-1, X, Y)
 one_pair = X[result[1]]
 two_pair = X[result[2]]
 print 'the closest distance and its index:',result
 print 'the closest two point is:', one_pair, two_pair
 print 'check the result:', r.distance(one_pair, two_pair)
+truth = r.brute_force(X)
+three_pair = X[truth[1]]
+four_pair = X[truth[2]]
+print 'the result derived by brute_force method:', truth
 X = np.transpose(X)
 plt.figure()
 plt.plot(X[0], X[1], 'ro')
 plt.plot([one_pair[0], two_pair[0]], [one_pair[1], two_pair[1]], 'b-', lw=2)
+plt.plot([three_pair[0], four_pair[0]], [three_pair[1], four_pair[1]], 'k-', lw=2)
 plt.show()
