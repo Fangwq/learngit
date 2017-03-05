2fc4f4af9012f9a34b0639b828806f06f7f749e1
FWQ <shenzhizi124@gmail.com>
2017-03-05 10:37:11 +0800

    minimum number of scalar multiplications

--- dyn_pro_main.py
+++ dyn_pro_main.py
@@ -4,6 +4,10 @@ from dynamic_program import dynamic_programming
 
 stringA = 'xyxxzxyzxy'
 stringB = 'zxzyyzxxyxxz'
-a = dynamic_programming(stringA, stringB)
+a = dynamic_programming(stringA)
 r = a.common_sequence(stringA, stringB)
 print r
+M = [5,10,4,6,10,2]
+b = dynamic_programming(M)
+result = b.matchain(M)
+print result
--- dynamic_program.py
+++ dynamic_program.py
@@ -2,11 +2,11 @@
 import numpy as np
 
 class dynamic_programming(object):
-	def __init__(self, A, B):
+	def __init__(self, A):
 		self.A = A
-		self.B = B
+		# self.B = B
 
-	def common_sequence(self, A, B):
+	def common_sequence(self, A, B):    #how to get the common sequence?
 		n = len(A)
 		m = len(B)
 		L = np.array([[0]*(m+1) for i in xrange(n+1)])    #(n+1)*(m+1) array
@@ -15,8 +15,22 @@ class dynamic_programming(object):
 			for j in xrange(1, m+1):
 				if A[i-1] == B[j-1]:
 					L[i, j] = L[i-1, j-1] + 1
-					print L
+					# print L
 				else:
 					L[i, j] = max(L[i, j-1], L[i-1, j])
-		print L
+		# print L
 		return L[n-1, m-1]
+
+	def matchain(self, R):
+		length = len(R)-1
+		C = np.array([[0]*(length) for i in xrange(length)])
+		for d in xrange(length - 1):
+			for i in xrange(length - d -1):
+				j = i + d +1
+				C[i, j] = 10**10
+				for k in xrange(i+1, j+1):
+					print C[i, j], C[i, k-1] + C[k, j] + R[i]*R[k]*R[j+1]
+					C[i, j] = min(C[i, j], C[i, k-1] + C[k, j] + R[i]*R[k]*R[j+1])
+		return C
+
+
