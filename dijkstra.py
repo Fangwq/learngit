4559ae45d4726def585ffc4159fe0c91705aa96d
FWQ <shenzhizi124@gmail.com>
2017-04-24 23:04:35 +0800

    the main

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,10 @@
 #-*- coding: utf-8 -*-   
 import dijkstra as di
-print di.main()
+import method_dijkstra as method_di 
+
+#in this case, I change the code in run.sh, 
+#so I comment the first line
+
+#print di.main()
+method_di.main()
+
